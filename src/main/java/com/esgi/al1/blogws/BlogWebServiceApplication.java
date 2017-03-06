18c2ba5cd599f53d49e1e1299e49ceb5d0f7e8c9
RomanBelic <noobek@live.fr>
2017-03-06 22:15:30 +0100

    hello

---
+++ src/main/java/com/esgi/al1/blogws/BlogWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.esgi.al1.blogws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/esgi/al1/blogws/BlogWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esgi.al1.blogws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

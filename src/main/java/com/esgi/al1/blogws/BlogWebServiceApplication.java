6849987de60b8d8e2c5200d924db3d4457b5ee7a
RomanBelic <noobek@live.fr>
2017-03-06 22:25:00 +0100

    hello

--- src/main/java/com/esgi/al1/blogws/BlogWebServiceApplication.java
+++ src/main/java/com/esgi/al1/blogws/BlogWebServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogWebServiceApplication {
+	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebServiceApplication.class, args);

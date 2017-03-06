c0ebc9d8b47bc185ea3055e5adbd9044ef8e6535
champCAR <gagoudechris@gmail.com>
2017-03-06 23:13:20 +0100

    Commit message from Chris

--- src/main/java/com/esgi/al1/blogws/BlogWebServiceApplication.java
+++ src/main/java/com/esgi/al1/blogws/BlogWebServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogWebServiceApplication {
 	//test
-
+	// Commit from Chris
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebServiceApplication.class, args);
 	}

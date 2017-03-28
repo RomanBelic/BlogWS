e8c52b957fdc6620dfad9f2abe9115c0246afe22
champCAR <gagoudechris@gmail.com>
2017-03-28 23:51:42 +0100

    Tests du application.properties.

--- src/test/java/com/esgi/al1/blogws/ConfigurationPropertiesIT.java
+++ src/test/java/com/esgi/al1/blogws/ConfigurationPropertiesIT.java
@@ -1,17 +1,25 @@
 package com.esgi.al1.blogws;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.jayway.restassured.RestAssured.when;
+import static org.junit.Assert.*;
 
 /**
  * Created by Chris GAGOUDE on 21/03/2017.
  */
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ConfigurationPropertiesIT {
+
 
     @Test
     public void should_get_mysql_properties(){
@@ -26,15 +34,15 @@ public class ConfigurationPropertiesIT {
                 .body("dbparam", CoreMatchers.is("?convertToNull&autoReconnect=true&characterEncoding=UTF-8&characterSetResults=UTF-8&allowMultiQueries=true"));
 
     }
+
     @Test
     public void should_get_web_properties(){
         when()
               .get("/Post")
         .then()
                 .statusCode(200)
-                .body("server", CoreMatchers.notNullValue())
-                .body("host", CoreMatchers.is("localhost"))
-                .body("server.port", CoreMatchers.is(2000));
+                .body("connectionstring.host", CoreMatchers.is("localhost"))
+                .body("connectionstring.webport", CoreMatchers.is(8090));
     }
 
 

ee704a9799758281d2bf4894d3257a7ccf8936a8
champCAR <gagoudechris@gmail.com>
2017-04-06 03:05:12 +0100

    Tests pour PostRepository et UserRepository.

--- src/test/java/com/esgi/al1/blogws/PostRepositoryIT.java
+++ src/test/java/com/esgi/al1/blogws/PostRepositoryIT.java
@@ -2,9 +2,8 @@ package com.esgi.al1.blogws;
 
 import com.esgi.al1.blogws.dao.PostRepository;
 import com.esgi.al1.blogws.models.Post;
-import com.esgi.al1.blogws.utils.DataBase;
 import com.esgi.al1.blogws.utils.Queries;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,26 +12,102 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by Chris GAGOUDE on 21/03/2017.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PostRepository.class)
 @ActiveProfiles("Test")
 @DataJpaTest
 public class PostRepositoryIT {
 
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    Queries queries;
+    Post post;
 
     public void init(){
+        post = new Post();
+        post.setDate(new Date());
+        post.setId(1);
+        post.setTitle("Post title");
+        post.setText("Post text");
+        post.setAuthorId(1);
     }
 
     @Test
-    public void should_get_post_data(){
+    public void should_save_post(){
+        int inserted = postRepository.insert(queries.InsertPost, post);
+        assertTrue( inserted > 0);
+    }
 
+    @Test
+    public void should_get_post_by_id(){
+        Post returnedPost = postRepository.get(queries.GetPost, post.getId());
+        assertThat(returnedPost.getId()).isEqualTo(post.getId());
+    }
+
+    @Test
+    public void should_get_post_by_authorId(){
+        Post returnedPost = postRepository.get(queries.GetPost, post.getAuthorId());
+        assertThat(returnedPost.getAuthorId()).isEqualTo(post.getAuthorId());
+    }
+
+    @Test
+    public void should_get_post_by_date(){
+        Post returnedPost = postRepository.get(queries.GetPost, post.getDate());
+        assertThat(returnedPost.getDate()).isEqualTo(post.getDate());
+    }
+
+    @Test
+    public void should_get_post_by_title(){
+        Post returnedPost = postRepository.get(queries.GetPost, post.getTitle());
+        assertThat(returnedPost.getTitle()).isEqualTo(post.getTitle());
+    }
+
+    @Test
+    public void should_update_post_authorId_by_postId(){
+        post.setAuthorId(2);
+        int confirmUpdated = postRepository.updateOrDelete(queries.UpdatePost, post.getId());
+        assertThat(confirmUpdated).isNotNull();
+    }
+
+    @Test
+    public void should_update_postDate_by_postId(){
+        post.setDate(new Date(2017, 12, 31));
+        int confirmUpdated = postRepository.updateOrDelete(queries.UpdatePost, post.getId());
+        assertThat(confirmUpdated).isNotNull();
+    }
+
+    @Test
+    public void should_update_postTitle_by_postId(){
+        post.setTitle("Post title modified");
+        int confirmUpdated = postRepository.updateOrDelete(queries.UpdatePost, post.getId());
+        assertThat(confirmUpdated).isNotNull();
+    }
+
+    @Test
+    public void should_get_all_posts(){
+        List<Post> listPosts = postRepository.getAll(queries.GetAllPosts);
+        assertThat(listPosts).isNotEmpty();
+    }
+
+    @Test
+    public void should_get_all_posts_by_authorId(){
+        List<Post> listPosts = postRepository.getAll(queries.GetAllPosts, 1);
+        assertThat(listPosts).isNotEmpty();
+    }
+
+    @Test
+    public void should_get_all_posts_by_date(){
+        List<Post> listPosts = postRepository.getAll(queries.GetAllPosts, new Date());
+        assertThat(listPosts).isNotEmpty();
     }
 }
---
+++ src/test/java/com/esgi/al1/blogws/UserRepositoryIT.java
@@ -0,0 +1,92 @@
+package com.esgi.al1.blogws;
+
+import com.esgi.al1.blogws.dao.PostRepository;
+import com.esgi.al1.blogws.dao.UserRepository;
+import com.esgi.al1.blogws.models.Post;
+import com.esgi.al1.blogws.models.UserType;
+import com.esgi.al1.blogws.utils.Queries;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.esgi.al1.blogws.models.User;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Chris GAGOUDE on 01/04/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserRepository.class)
+@ActiveProfiles("Test")
+public class UserRepositoryIT {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    Queries queries;
+    User userAdmin;
+    User user;
+
+    public void init(){
+        userAdmin = new User();
+        userAdmin.setId(1);
+        userAdmin.setId(1);
+        userAdmin.setDateCreated(new Date());
+        userAdmin.setName("Luiz");
+        userAdmin.setLastName("Enrique");
+        userAdmin.setType(UserType.Admin);
+
+        user = new User();
+        user.setId(2);
+        user.setId(2);
+        user.setDateCreated(new Date());
+        user.getName("Junior");
+        user.getLastName("Neymar");
+        user.setType(UserType.User);
+    }
+
+    @Test
+    public void should_save_user(){
+        int insertedUserAdmin = userRepository.insert(queries.InsertUser, userAdmin);
+        int insertedUser = userRepository.insert(queries.InsertUser, user);
+
+        assertTrue( insertedUserAdmin > 0 && insertedUser > 0);
+    }
+
+    @Test
+    public void should_get_user_admin_by_id(){
+        User returnedUser = userRepository.get(queries.GetUser, userAdmin.getId());
+        assertThat(returnedUser.getId()).isEqualTo(userAdmin.getId());
+    }
+
+    @Test
+    public void should_get_user_by_id(){
+        User returnedUser = userRepository.get(queries.GetUser, user.getId());
+        assertThat(returnedUser.getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    public void should_get_all_users(){
+        List<User> listUsers = userRepository.getAll(queries.GetAllUsers);
+        assertThat(listUsers).isNotEmpty();
+    }
+
+    @Test
+    public void should_get_all_users_by_type_admin(){
+        List<User> listUsers = userRepository.getAll(queries.GetAllUsers, UserType.Admin);
+        assertThat(listUsers).isNotEmpty();
+    }
+
+    @Test
+    public void should_get_all_users_by_type_who_are_not_admin(){
+        List<User> listUsers = userRepository.getAll(queries.GetAllUsers, UserType.User);
+        assertThat(listUsers).isNotEmpty();
+    }
+
+}

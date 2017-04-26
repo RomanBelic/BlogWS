d4d53a81b4852c02aa9bfb3e8037eb76a1044171
champCAR <gagoudechris@gmail.com>
2017-04-26 10:01:59 +0100

    Tests Controllers

--- src/test/java/com/esgi/al1/blogws/PostControllerIT.java
+++ src/test/java/com/esgi/al1/blogws/PostControllerIT.java
@@ -1,5 +1,6 @@
 package com.esgi.al1.blogws;
 
+import com.esgi.al1.blogws.controllers.PostController;
 import com.esgi.al1.blogws.models.Post;
 import com.esgi.al1.blogws.models.PostDTO;
 import com.jayway.restassured.RestAssured;
@@ -7,8 +8,12 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -16,20 +21,30 @@ import java.util.Date;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static com.jayway.restassured.http.ContentType.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Chris GAGOUDE on 21/03/2017.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)    // Will listen on default port 8090
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
 public class PostControllerIT {
 
-    @Value("${connectionstring.webport}")
+    //@Autowired
+    PostController postController;
+
+    @LocalServerPort
     int port;
 
     @Before
     public void restassuredinit(){
         RestAssured.port = port;
+    }
+
+    @Test
+    public void soulhd_load_web_context(){
+        assertThat(postController).isNotNull();
     }
 
     @Test
@@ -107,6 +122,7 @@ public class PostControllerIT {
 
     }
 
+    @Test
     public void should_not_create_post_when_title_and_text_are_null(){
         PostDTO postDTO = PostDTO.builder()
                 .id(2).build();
